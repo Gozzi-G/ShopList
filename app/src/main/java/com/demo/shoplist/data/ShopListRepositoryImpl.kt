d3529ed8f9281e950f931a29665e33b2d5d995f5
Yury <struck.yury@gmail.com>
2021-08-14 16:38:39 +0300

    Add implementation of ShopListRepository

--- app/src/main/java/com/demo/shoplist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/demo/shoplist/data/ShopListRepositoryImpl.kt
@@ -2,5 +2,38 @@ package com.demo.shoplist.data
 
 import com.demo.shoplist.domain.ShopItem
 import com.demo.shoplist.domain.ShopListRepository
+import java.lang.RuntimeException
 
-object ShopListRepositoryImpl
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID ) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItemUseCase(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+}
--- app/src/main/java/com/demo/shoplist/domain/InsertShopItemUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.demo.shoplist.domain
-
-class InsertShopItemUseCase(private val shopListRepository: ShopListRepository) {
-    fun insertShopItem(shopItem: ShopItem) {
-        shopListRepository.insertShopItem(shopItem)
-    }
-}
