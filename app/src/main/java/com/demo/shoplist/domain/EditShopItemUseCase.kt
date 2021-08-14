326a679e3dc30887eb29836094861e94aa5c47f1
Yury <struck.yury@gmail.com>
2021-08-14 14:36:04 +0300

    Add UseCases of ShopItem with functions without implementation

---
+++ app/src/main/java/com/demo/shoplist/domain/EditShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.demo.shoplist.domain
+
+class EditShopItemUseCase {
+    fun editShopItemUseCase(shopItem: ShopItem) {
+        TODO()
+    }
+}
--- app/src/main/java/com/demo/shoplist/domain/GetShopListItemUseCase.kt
+++ app/src/main/java/com/demo/shoplist/domain/GetShopListItemUseCase.kt
@@ -1,6 +1,6 @@
 package com.demo.shoplist.domain
 
-class GetShopListItemUseCase {
+class GetShopListItemUseCase(private val shopListRepository: ShopListRepository) {
     fun getShopList(): List<ShopItem> {
         TODO()
     }
