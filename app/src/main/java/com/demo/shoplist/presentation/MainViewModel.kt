6b50d90f389b26578d713cb9f75962570a28664f
Yury <struck.yury@gmail.com>
2021-08-14 17:45:43 +0300

    Add presentation layer

---
+++ app/src/main/java/com/demo/shoplist/presentation/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.demo.shoplist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.demo.shoplist.data.ShopListRepositoryImpl
+import com.demo.shoplist.domain.*
+
+class MainViewModel: ViewModel() {
+
+}
